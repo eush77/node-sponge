c8582c1d2524859165a2658fca64ba55d544da1c
Eugene Sharygin <eush77@gmail.com>
2015-02-26 13:41:52 +0300

    Add index.js and cli.js

---
+++ cli.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+var sponge = require('./');
+
+
+var usage = function () {
+  return 'Usage:  sponge [<file>]';
+};
+
+process.exitCode = (function (argv) {
+  switch (argv.length) {
+    case 0:
+      process.stdin
+        .pipe(sponge())
+        .pipe(process.stdout);
+      return 0;
+
+    case 1:
+      if (argv[0] == '--help') {
+        console.log(usage());
+      }
+      else {
+        process.stdin.pipe(sponge(argv[0]));
+      }
+      return 0;
+
+    default:
+      console.error(usage());
+      return 1;
+  }
+}(process.argv.slice(2)));
---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var concat = require('concat-stream'),
+    through = require('through2'),
+    duplexer = require('duplexer2');
+
+var fs = require('fs');
+
+
+module.exports = function (file) {
+  if (file != null) {
+    return concat(function (data) {
+      fs.createWriteStream(file).end(data);
+    });
+  }
+  else {
+    var readable = through();
+    var writable = concat(readable.write.bind(readable));
+    return duplexer(writable, readable);
+  }
+};
